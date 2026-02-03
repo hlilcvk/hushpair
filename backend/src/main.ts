85cf5e5b21f7262beabe529e2717a3b99fafbc3a
hlilcvk <hlilcvk@gmail.com>
2026-02-03 19:16:25 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as helmet from 'helmet';
-import * as compression from 'compression';
+import helmet from 'helmet';
+import compression from 'compression';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
